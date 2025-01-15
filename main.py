26552ea017b518a15708b0fef9f2789e94c4b20e
FREDYK1 <fkwekukankam@gmail.com>
2025-01-15 11:59:44 +0000

    Combine terms with only one difference in bit

--- main.py
+++ main.py
@@ -19,6 +19,18 @@ def group_minterms(minterms, num_vars):
         groups[ones_count].append(binary)
     return groups
 
+def combine_terms(term1, term2):
+    # Combine two terms if only one bit is different
+    combined = ""
+    diff_count = 0
+    for bit1, bit2 in zip(term1, term2):
+        if bit1 != bit2:
+            combined += '-'
+            diff_count += 1
+        else:
+            combined += bit1
+    # Return combined term if only one bit is different
+    return combined if diff_count == 1 else None
 
 
 # minterms = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
