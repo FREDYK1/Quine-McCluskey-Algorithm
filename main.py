a45f8094d85bc802a0868b7e0a1c1099f187b2e7
FREDYK1 <fkwekukankam@gmail.com>
2025-01-15 12:12:23 +0000

    Find Essential Prime Implicants

--- main.py
+++ main.py
@@ -32,6 +32,32 @@ def combine_terms(term1, term2):
     # Return combined term if only one bit is different
     return combined if diff_count == 1 else None
 
+def find_prime_implicants(minterms, num_vars):
+    groups = group_minterms(minterms, num_vars)
+    prime_implicants = set()
+    # Combine terms in adjacent groups
+    while groups:
+        new_groups = {}
+        used = set()
+        for i in sorted(groups.keys()):
+            if i+1 in groups:
+                for term1 in groups[1]:
+                    for term2 in groups[i+1]:
+                        combined = combine_terms(term1, term2)
+                        if combined:
+                            used.add(term1)
+                            used.add(term2)
+                            ones_count = count_ones(combined)
+                            if ones_count not in new_groups:
+                                new_groups[ones_count] = []
+                            new_groups[ones_count].append(combined)
+        # Add unused terms to prime implicants
+        for group in groups.values():
+            for term in group:
+                if term not in used:
+                    prime_implicants.add(term)
+        groups = new_groups
+    return prime_implicants
 
 # minterms = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
 # # num_vars = 4
